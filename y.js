bb03d52190a5155fb187b81dd28d0b6efe2d01ba
Elias G. Schneevoigt <eliasgs@gmail.com>
2013-12-20 21:58:37 +0100

    Change the settings api

--- y.js
+++ y.js
@@ -1,13 +1,21 @@
 var util = require('util');
-module.exports = function y() {
+module.exports = y;
+
+function y() {
   var err = new Error();
   Error.captureStackTrace(err, arguments.callee);
   var stack = err.stack.split('\n');
   console.log('\u001b[36m' + new Date().toJSON() + stack[1] + '\u001b[39m');
 
+  var depth = y.settings && y.settings.depth || 2;
   for (var i=0; i<arguments.length; i++){
     var type =  Object.prototype.toString.call(arguments[i]);
     console.log('\u001b[4m' + type + '\u001b[24m');
-    console.log(util.inspect(arguments[i], true, y.depth, true));
+    console.log(util.inspect(arguments[i], true, depth, true));
   }
+}
+
+y.set = function (settings) {
+  y.settings = settings;
+  return y;
 };
